a8acd0f87aa2f64ceb84a7dd41dd875c28c6edb0
Lauren Le <laurenjle@fb.com>
2019-08-02 14:01:49 -0700

    added databaseURI, appID, masterKey, and serverURL in index.js

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://heroku_cm03672n:nos14mprcq3npfcdrj3u0355jc@ds139801.mlab.com:39801/heroku_cm03672n',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'flexx',
+  masterKey: process.env.MASTER_KEY || 'flexx', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'https://seam-flexx.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
