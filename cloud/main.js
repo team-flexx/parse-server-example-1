77123e8823ee8fc6ec509c97afb73bc65d9c7545
Lauren Le <laurenjle@fb.com>
2019-08-02 14:28:32 -0700

    attempt to access Parse data

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,11 @@
 Parse.Cloud.define('hello', function(req, res) {
   return 'not hi';
 });
+
+
+Parse.Cloud.define("gettingData", async (request) => {
+  const query = new Parse.Query("SMJobs");
+  query.equalTo("companyName", request.params.companyName);
+  const results = await query.find();
+  return results;
+});
