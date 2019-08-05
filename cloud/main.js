86cee8280890a1e1f51b830c60033138ce877adf
Lauren Le <laurenjle@fb.com>
2019-08-05 14:59:39 -0700

    changed cloud function to store data into SMMatches on parse if match was found. need to deploy this and see if it works when called locally

--- cloud/main.js
+++ cloud/main.js
@@ -18,18 +18,28 @@ Parse.Cloud.afterSave("performMatching", (request) => { //pass in the whole jobl
   const query = new Parse.Query("SMEmployerSwipeRight"); 
 
   //get params from applicant swipe right (jobID, userID) 
-  query.equalTo("jobID", request.params.jobID) && query.equalTo("author, request.params.author");
-  
+  query.equalTo("jobID", request.params.jobID) && query.equalTo("applicantID, request.params.applicantID");
   
   const matchExists = await query.find(); //should be true or false
+
   if (matchExists){
-    //return the whole joblisting object by getting it from the SMJobListing class based on the JobID
-    const getJobQuery = newParse.Query("SMJobListing");
-    getJobQuery.equalTo("jobID");
-    return getJobQuery.find();
-  }
-  else{
-    return "no match found";
+  //TODO: save a row to matched table in parse
+  const SMMatches = Parse.Object.extend("SMMatches");
+  const aNewMatch = new SMMatches();
+
+  aNewMatch.set("user", "test");
+  aNewMatch.set("employer", "test");
+  aNewMatch.set("matchedJobID", "test");
+
+  aNewMatch.save().then((aNewMatch) => {
+    // Execute any logic that should take place after the object is saved.
+    alert('New object created with objectId: ' + aNewMatch.id);
+  }, (error) => {
+    // Execute any logic that should take place if the save fails.
+    // error is a Parse.Error with an error code and message.
+    alert('Failed to create new object, with error code: ' + error.message);
+  });
+  
   }
 });
 
