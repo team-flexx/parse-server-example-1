e065e02d71d590aecf72079af5b4339c953f1fbd
Lauren Le <laurenjle@fb.com>
2019-08-06 16:52:24 -0700

    test

--- cloud/main.js
+++ cloud/main.js
@@ -10,16 +10,14 @@ Parse.Cloud.define("gettingData", async (request) => {
   return results[0].get("applicantRejections");
 });
 
-Parse.Cloud.define('one', function(req, res) {
-  return 'function called one';
-});
-
-//an applicant swipe is saved, so we should look into that jobCard's employer, then
-//and check: (is userID present &  jobID present?) if yes, then send that data to the
-//matches view controller
 
 //function = the function to run after save, which takes one paramter, Parse.Cloud.TriggerRequest
-Parse.Cloud.afterSave(Parse.SMApplicantSwipeRight, 
-  function one(){}
+Parse.Cloud.afterSave(Parse.SMApplicantSwipeRight,(request) =>{
+  const query = new Parse.Query("SMEmployerSwipeRight");
+  query.equalTo("jobID", request.params.jobID)
+  const matchExistsArray = await query.find();
+  return matchExistsArray;
+
+}
 );
 
