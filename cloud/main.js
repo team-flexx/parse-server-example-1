f64038e602111bce11d9e2c837ac40fe4a1c5f1b
Lauren Le <laurenjle@fb.com>
2019-08-05 17:41:01 -0700

    testing matching funciton

--- cloud/main.js
+++ cloud/main.js
@@ -15,6 +15,34 @@ Parse.Cloud.define("gettingData", async (request) => {
 //matches view controller
 
 Parse.Cloud.afterSave(Parse.SMApplicantSwipeRight, (request) => { //pass in the whole joblisting
-  return "something was saved";
+  //get params from applicant swipe right (jobID, userID)
+  //go to employer's swipe and get jobID userID and check. NOTE: Need to add in userID into Parse
+  const query = new Parse.Query("SMEmployerSwipeRight"); 
+
+  query.equalTo("jobID", request.params.jobID) && query.equalTo("applicantID, request.params.applicantID");
+  
+  const matchExistsArray = await query.find(); //this is an array of stuff that has the right one, and it SHOULD be one
+  MediaKeySystemAccess.out.print(matchExistsArray[0]); 
+  if (matchExistsArray != undefined){
+  //TODO: save a row to matched table in parse
+  const SMMatches = Parse.Object.extend("SMMatches");
+  const aNewMatch = new SMMatches();
+
+  aNewMatch.set("user", "test");
+  aNewMatch.set("employer", "test");
+  aNewMatch.set("matchedJobID", "test");
+
+
+  aNewMatch.save().then((aNewMatch) => {
+    // Execute any logic that should take place after the object is saved.
+    alert('New object created with objectId: ' + aNewMatch.id);
+  }, (error) => {
+    // Execute any logic that should take place if the save fails.
+    // error is a Parse.Error with an error code and message.
+    alert('Failed to create new object, with error code: ' + error.message);
+  });
+  
+  }
+
 });
 
