e475067f50215d05c7d7015efda5ceade1b51f34
Lauren Le <laurenjle@fb.com>
2019-08-06 15:58:15 -0700

    another test

--- cloud/main.js
+++ cloud/main.js
@@ -13,11 +13,20 @@ Parse.Cloud.define("gettingData", async (request) => {
 //an applicant swipe is saved, so we should look into that jobCard's employer, then
 //and check: (is userID present &  jobID present?) if yes, then send that data to the
 //matches view controller
-
-Parse.Cloud.afterSave(Parse.SMApplicantSwipeRight, (request, response) => {//pass in the whole joblisting
-  //get params from applicant swipe right (jobID, userID)
-  //go to employer's swipe and get jobID userID and check. NOTE: Need to add in userID into Parse
-  console.log("does .afterSave get triggered after something is swiped right?");
-
+Parse.Cloud.define("tryFunction", async (request) =>{
+  Parse.Cloud.afterSave(Parse.SMApplicantSwipeRight, (request, response) => {//pass in the whole joblisting
+    //get params from applicant swipe right (jobID, userID)
+    //go to employer's swipe and get jobID userID and check. NOTE: Need to add in userID into Parse
+    //console.log("does .afterSave get triggered after something is swiped right?");
+    
+    if (!request.object.existed()){
+      console.log("job didn't exist");
+    }
+    else{
+      console.log("job existed");
+    }
+  });
+  return "does anything work??? ";
 });
+
 
