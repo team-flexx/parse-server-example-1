0bd8900efd1abdde4f2a0ce5499b7e93617db190
Lauren Le <laurenjle@fb.com>
2019-08-06 16:38:04 -0700

    defined function outside of .afterSave

--- cloud/main.js
+++ cloud/main.js
@@ -10,15 +10,16 @@ Parse.Cloud.define("gettingData", async (request) => {
   return results[0].get("applicantRejections");
 });
 
+Parse.Cloud.define('one', function(req, res) {
+  return 'function called one';
+});
+
 //an applicant swipe is saved, so we should look into that jobCard's employer, then
 //and check: (is userID present &  jobID present?) if yes, then send that data to the
 //matches view controller
 
 //function = the function to run after save, which takes one paramter, Parse.Cloud.TriggerRequest
 Parse.Cloud.afterSave(Parse.SMApplicantSwipeRight, 
-  function(request)
-  {
-   return "something";
-  }
+  function one(){}
 );
 
