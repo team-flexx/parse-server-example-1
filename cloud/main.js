192bbd8c1bd64d52435a75f9fc665f652ec51b1d
Lauren Le <laurenjle@fb.com>
2019-08-08 17:57:28 -0700

    using .map

--- cloud/main.js
+++ cloud/main.js
@@ -116,7 +116,10 @@ Parse.Cloud.define("getMatchedData", async (request) => {
 
   const matches = await query.find();
   // logger.info("matches to return: " + JSON.stringify(matches));
-  const unwrappedMatches = matches.map(match => match["jobPointer"]);
-  logger.info("matches to return: " + JSON.stringify(unwrappedMatches));
+  const unwrappedMatches = matches.map(match => {
+    logger.info("match: " + JSON.stringify(match));
+    return match["jobPointer"];
+  });
+  // logger.info("matches to return: " + JSON.stringify(unwrappedMatches));
   return unwrappedMatches;
 });
